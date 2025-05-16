4e418fabcfbcf0a128ed769cd5899da054bc0e9e
Leester <128757198+Leescifer@users.noreply.github.com>
2025-05-16 13:51:11 +0800

    Feat: fix the email ui

--- client/src/pages/AdminAuth/email.jsx
+++ client/src/pages/AdminAuth/email.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import style from '@styles/adminAuth.module.scss'
 
 const Email = ({ next, prev, handleChange, data }) => {
   return (
-    <div>
-      <h2>Check your email for a verification link</h2>
+    <div className={style.emailBox}>
+      <h2 className={style.emailTitle}>Check your email for a
+        verification link</h2>
       <input
         type="email"
         placeholder="Enter your email"
         value={data.email}
         onChange={(e) => handleChange("email", e.target.value)}
       />
-      <p>If the email doesn’t arrive, check your spam folder.</p>
-      <button onClick={prev}>Back</button>
+      <p className={style.emailDesc}>If the email doesn't arrive within a few minutes, 
+        please be sure to check your spam or junk folder.</p>
       <button onClick={next}>Continue</button>
+      <button onClick={prev} className={style.back}>Back</button>
     </div>
   );
 };
