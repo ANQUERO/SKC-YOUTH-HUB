d304a4a4f7539035bdf9a4716e839a23e5aaed91
Leester <128757198+Leescifer@users.noreply.github.com>
2025-07-17 23:49:47 +0800

    finalizing the signin and signup adding dummies for examples

--- server/src/controller/youth.controller.js
+++ server/src/controller/youth.controller.js
@@ -81,16 +81,16 @@ export const show = async (req, res) => {
             attachments,
             household
         ] = await Promise.all([
-            pool.query('SELECT * FROM sk_youth'),
-            pool.query('SELECT * FROM sk_youth_name'),
-            pool.query('SELECT * FROM sk_youth_location'),
-            pool.query('SELECT * FROM sk_youth_gender'),
-            pool.query('SELECT * FROM sk_youth_info'),
-            pool.query('SELECT * FROM sk_youth_demographics'),
-            pool.query('SELECT * FROM sk_youth_survey'),
-            pool.query('SELECT * FROM sk_youth_meeting_survey'),
-            pool.query('SELECT * FROM sk_youth_attachments'),
-            pool.query('SELECT * FROM sk_youth_household'),
+            pool.query('SELECT * FROM sk_youth WHERE youth_id = $1', [youth_id]),
+            pool.query('SELECT * FROM sk_youth_name WHERE youth_id = $1', [youth_id]),
+            pool.query('SELECT * FROM sk_youth_location WHERE youth_id = $1', [youth_id]),
+            pool.query('SELECT * FROM sk_youth_gender WHERE youth_id = $1', [youth_id]),
+            pool.query('SELECT * FROM sk_youth_info WHERE youth_id = $1', [youth_id]),
+            pool.query('SELECT * FROM sk_youth_demographics WHERE youth_id = $1', [youth_id]),
+            pool.query('SELECT * FROM sk_youth_survey WHERE youth_id = $1', [youth_id]),
+            pool.query('SELECT * FROM sk_youth_meeting_survey WHERE youth_id = $1', [youth_id]),
+            pool.query('SELECT * FROM sk_youth_attachments WHERE youth_id = $1', [youth_id]),
+            pool.query('SELECT * FROM sk_youth_household WHERE youth_id = $1', [youth_id]),
         ]);
 
         if (youth.rows.length === 0) {
--- server/src/dumimes/dummy.js
+++ server/src/dumimes/dummy.js
@@ -1,3 +1,5 @@
+//Admin Exanple Signup
+
 // {
 //     "email": "leester@gmail.com",
 //         "password": "Leeste!1",
@@ -5,4 +7,36 @@
 //                 "last_name": "Cruspero",
 //                     "role": "super_sk_admin",
 //                         "position": "Chairman"
+// }
+
+//Youth Exanple Signup
+
+// {
+//   "email": "ter@gmail.com",
+//   "password": "Leeste!1",
+//   "first_name": "Ter",
+//   "middle_name": "A",
+//   "last_name": "Crus",
+//   "suffix": "Jr",
+//   "region": "Region VII",
+//   "province": "Cebu",
+//   "municipality": "Cordova",
+//   "barangay": "Catarman",
+//   "purok_id": "6",
+//   "gender": "male",
+//   "age": 22,
+//   "contact": "09123456789",
+//   "birthday": "2003-10-11",
+//   "civil_status": "Single",
+//   "youth_age_gap": "18–24",
+//   "youth_classification": "Working school youth",
+//   "educational_background": "College Grad",
+//   "work_status": "Employed",
+//   "registered_voter": "yes",
+//   "registered_national_voter": "yes",
+//   "vote_last_election": "yes",
+//   "attended": "Yes",
+//   "times_attended": 3,
+//   "reason_not_attend": "Busy",
+//   "household": "Pedro"
 // }
