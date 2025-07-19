05fe895b226e9197c9d2ccc9590c5a1a47308cd4
Leester <128757198+Leescifer@users.noreply.github.com>
2025-07-19 14:49:41 +0800

    updates

---
+++ client/src/pages/ForgotPasword/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from '@styles/forgot.module.scss';
+
+
+const ForgotPassword = () => {
+    return (
+        <section className='container'>
+
+
+
+
+
+        </section>
+    ); 
+}
+
+export default ForgotPassword
