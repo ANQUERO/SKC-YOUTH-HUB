8c173d6de794eff0f614094db44a887d373ea6b9
Leester <128757198+Leescifer@users.noreply.github.com>
2025-07-21 12:45:32 +0800

    update:Tiwason nalang inig uli

---
+++ client/src/hooks/useSignin.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import axiosInstance from '@lib/axios';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '@context/AuthContext';
+import validate from 'validate.js'
+
+
+
+
+
+const useLogin = () => {
+    const navigate = useNavigate();
+    const { setAuthUser } = useAuthContext();
+
+}
+
