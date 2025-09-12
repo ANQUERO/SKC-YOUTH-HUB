1e6af0ff97f92512ad12896d241965a4c743ef9f
lcruspero-dev <lcruspero@csvnow.com>
2025-09-13 03:34:09 +0800

    update:Landing Page footer section

--- client/src/pages/LandingPage/sections/footer.jsx
+++ client/src/pages/LandingPage/sections/footer.jsx
@@ -1,26 +1,77 @@
 import React from 'react';
 import style from '@styles/landingpage.module.scss';
 import Logo from '@images/logo.jpg'
+import { Facebook, Instagram, Youtube } from "lucide-react";
 
 const Footer = () => {
     return (
         <footer className={style.footer} id="footer">
-            <div className={style.footerLogo}>
-                <img src={Logo} alt="Logo" />
-                <h1 className={style.logoText}>SKC</h1>
+
+            <h1 className={style.tagline}>
+                kabataan ang pag-asa ng bayan,
+                <span className={style.subTagline}>
+                    let's turn that hope into action
+                </span>
+            </h1 >
+
+            <div className={style.footerLinks}>
+
+                <div className={style.logo}>
+
+                    <img
+                        src={Logo}
+                        alt="Logo"
+                        className={style.logoImage}
+                    />
+
+                    <h1 className={style.logoText}>SKC</h1>
+
+                </div>
+
+                <ul className={style.navLinks}>
+                    <li className={style.links}>
+                        About
+                    </li>
+                    <li className={style.links}>
+                        Officials
+                    </li>
+                    <li className={style.links}>
+                        Discover
+                    </li>
+                    <li className={style.links}>
+                        Location
+                    </li>
+                </ul>
+
+                <div className={style.socialLinks}>
+                    <ul className={style.social}>
+                        <li className={style.socLinks}>
+                            <a href="">
+                                <Facebook />
+                            </a>
+                        </li>
+                        <li className={style.socLinks}>
+                            <a href="">
+                                <Instagram />
+                            </a>
+                        </li>
+                        <li className={style.socLinks}>
+                            <a href="">
+                                <Youtube />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
 
             </div>
 
-            <ul className={style.links}>
-                <li><a href="#">Facebook</a></li>
-                <li><a href="#">FAQ's</a></li>
-                <li><a href="#">Help</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms of Service</a></li>
-            </ul>
 
-            <p>&copy; 2025 SK Catarman. All rights reserved.</p>
-        </footer>
+
+
+
+
+        </footer >
     );
 };
 
