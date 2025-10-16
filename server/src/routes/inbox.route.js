2fd4cf53b7ced258aa7c1909bce027e58f278433
Leester <128757198+Leescifer@users.noreply.github.com>
2025-10-16 16:55:24 +0800

    updates

--- server/src/routes/inbox.route.js
+++ server/src/routes/inbox.route.js
@@ -12,18 +12,24 @@ export const index  = async (req, res) => {
         const result = await pool.query(
             `
             SELECT 
-            f.form_id,
-            f.title,
-            f.description,
-            f.is_hidden,
-            f.created_at,
-            f.updated_at,
-            n.first_name,            
-            n.middle_name,
-            n.last_name,
-            n.suffix,
+                f.form_id,
+                f.title,
+                f.description,
+                f.is_hidden,
+                f.created_at,
+                f.updated_at,
+                o.official_id,
+                o.official_position,
+                n.first_name,            
+                n.middle_name,
+                n.last_name,
+                n.suffix,
+                FROM forms f 
+                INNER JOIN sk_official 
             `
-        )
+        );
+
+
     } catch (error) {
         
     }
